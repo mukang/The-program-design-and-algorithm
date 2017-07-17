2fef5a8d591dc9c31c1a3df3e81eb4d84e8cf61d
穆康 <mukang@buluo-gs.com>
2017-07-17 11:18:16 +0800

    修改03-奇偶排序②中出现的bug

--- 03-奇偶排序②/03-奇偶排序②/main.cpp
+++ 03-奇偶排序②/03-奇偶排序②/main.cpp
@@ -33,7 +33,7 @@ int main(int argc, const char * argv[]) {
     
     // 冒泡，不断比较相邻的两个数，如果顺序错了，那么就交换
     for (int i=0; i<n-1; i++) {
-        for (int j=0; j<n-i; j++) {
+        for (int j=1; j<n-i; j++) {
             // 与刚才的冒泡排序不同，我们不只是通过较数字的大小决定顺序
             // 如果左边的为偶数，右边的为奇数，那么顺序也需要颠倒
             bool leftIsEven = a[j-1] % 2 == 0;
