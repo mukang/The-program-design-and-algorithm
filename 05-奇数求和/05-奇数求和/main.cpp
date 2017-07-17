359d4044cd2e3021461da146e7dcb840851a80fb
穆康 <mukang@buluo-gs.com>
2017-07-17 19:08:15 +0800

    新增练习

---
+++ 05-奇数求和/05-奇数求和/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  05-奇数求和
+//
+//  Created by 穆康 on 2017/7/17.
+//  Copyright © 2017年 杭州部落公社科技有限公司. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
